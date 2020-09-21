a5529c446f5415be34b699cf73835be325e33561
DoYourLove <xyf15620168540@163.com>
2020-09-21 22:42:47 +0800

    [XiaoYiFan] feat : add handle input logic

--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.message !== "") {
+      this.props.handleInput(this.state.message)
+      this.setState({
+        message: ""
+      })
+    }
+
+  }
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input type="text" onChange={this.handleChange} value={this.state.message} />
+        <button onClick={this.handleSubmit} type="button" >Send</button>
       </footer>
     );
   }
